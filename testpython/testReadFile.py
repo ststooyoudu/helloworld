bfddad1723b0755568c1b530876cd74e3dd443b7
ststooyoudu <876955293@qq.com>
2022-05-23 17:02:23 +0800

    Update testReadFile.py

--- testpython/testReadFile.py
+++ testpython/testReadFile.py
@@ -13,20 +13,22 @@ class DDL:
                 Alist.append(lineTemp)
         self.Story = (Alist[3].split(':'))[-1]
         self.DTS = (Alist[4].split(':'))[-1]
-        self.Author_1 = '_'.join(((Alist[5].split(':'))[-1]).split('_'))[:-1]   #需求编号
+        self.Author_1 = '_'.join((((Alist[5].split(':'))[-1]).split('_'))[:-1])   #需求编号
         self.Author_2 = ((Alist[5].split(':'))[-1]).split('_')[-1]     #开发名字
         self.Name = (Alist[6].split(':'))[-1]
         self.Date = (Alist[7].split(':'))[-1]
         self.Remark = (Alist[8].split(':'))[-1]
         self.DDLContent = '\n'.join(Alist[11:])
         self.DDLAllContent = '\n'.join(Alist[:])
-        self.userName = ((file.split('\\')[-1]).split('-'))[1]     #数据库名
+        self.userName = ((file.split('\\')[-1]).split('-'))[1]   
         self.Alist=Alist
         self.Create = (Alist[11].split(' ')[0].lower()=='create')   #判断文件是否是create
+
 userDict={'OM':'orders','IM':'im','PM':'bespub','SUBSCRIPTION':'subs','BESCHARGE':'bescharge'}
 ddl1=DDL(file)
 #print(ddl1.DDLContent)
-# print(ddl1.Author_1)
+#print(ddl1.Author_1)
 # print(ddl1.Author_2)
 # print(ddl1.Create)
 #print(ddl1.DDLAllContent)
+#print(ddl1.userName)
