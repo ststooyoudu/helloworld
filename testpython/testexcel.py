94e981912235b3b4660752067ed50e00c2f6156a
ststooyoudu <876955293@qq.com>
2022-05-23 18:47:37 +0800

    Update testexcel.py

--- testpython/testexcel.py
+++ testpython/testexcel.py
@@ -1,29 +1,40 @@
 import openpyxl
 from testReadFile import DDL
-wb = openpyxl.load_workbook('a.xlsx')
-#sheet = wb.sheetnames[0]
-sheet = wb.active
-print(sheet)
-#sheet1 = wb.create_sheet('myNewSheet')
-DDL1=DDL(r'/workspace/helloworld/00473-IM-(IM.OM.IM_RESERVE_BUSI_HIS_EX.REQ_2022_0206060_lwx997552.20220322103836.DDL)-Jiangsu_bes-swx552311.sql'
-)
-for column in range(sheet.max_column+1):
-    if column == 1:
-        sheet.cell(row=sheet.max_row+1,column=column).value=DDL1.Author_1
-    if column == 2:
-        sheet.cell(row=sheet.max_row,column=column).value= DDL1.Author_1
-    if column == 3:
-        sheet.cell(row=sheet.max_row,column=column).value= DDL1.Remark
-    if column == 5:
-        sheet.cell(row=sheet.max_row,column=column).value= 'ddl'
-    if column == 7:
-        sheet.cell(row=sheet.max_row,column=column).value= DDL1.DDLAllContent
-    if column == 8:
-        sheet.cell(row=sheet.max_row,column=column).value= DDL1.Author_2
-    if column == 11:
-        sheet.cell(row=sheet.max_row,column=column).value= '江苏bes'
-    if column == 14:
-        sheet.cell(row=sheet.max_row,column=column).value= '执行一次'    
-    if column == 15:
-        sheet.cell(row=sheet.max_row,column=column).value= '一般'  
-wb.save('a.xlsx')
+
+class TestExcel:
+    def __init__(self,xls_name,ddl_name):
+        self.wb = openpyxl.load_workbook(xls_name)
+        #sheet = wb.sheetnames[0]
+        self.sheet = self.wb.active
+        self.DDL1=DDL(ddl_name)
+        for column in range(self.sheet.max_column+1):
+            if column == 1:
+                self.sheet.cell(row=self.sheet.max_row+1,column=column).value=self.DDL1.Author_1
+            if column == 2:
+                self.sheet.cell(row=self.sheet.max_row,column=column).value= self.DDL1.Author_1
+            if column == 3:
+                self.sheet.cell(row=self.sheet.max_row,column=column).value= self.DDL1.Remark
+            if column == 5:
+                self.sheet.cell(row=self.sheet.max_row,column=column).value= 'ddl'
+            if column == 7:
+                self.sheet.cell(row=self.sheet.max_row,column=column).value= self.DDL1.DDLAllContent
+            if column == 8:
+                self.sheet.cell(row=self.sheet.max_row,column=column).value= self.DDL1.Author_2
+            if column == 11:
+                self.sheet.cell(row=self.sheet.max_row,column=column).value= '江苏bes'
+            if column == 14:
+                self.sheet.cell(row=self.sheet.max_row,column=column).value= '执行一次' 
+            if column == 15:
+                self.sheet.cell(row=self.sheet.max_row,column=column).value= '一般'  
+        self.wb.save('a.xlsx')
+
+# print(DDL1.DDLContent)
+#print(DDL1.Author_1)
+# print(DDL1.Author_2)
+# print(DDL1.Create)
+# print(DDL1.DDLAllContent)
+# print(DDL1.userName)
+
+#wb.save('a.xlsx')
+
+#test1=TestExcel('a.xlsx','/workspace/helloworld/testpython/00475-PM-(PM.PM.PM_TERMINAL_PRICE_EX.REQ_2022_0304100_y30020255.20220330164306.DDL)-Jiangsu_bes-zwx555221.sql')
