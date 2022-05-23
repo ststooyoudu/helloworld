230b91a2e3e445679abf670e3dbc362a262608a5
ststooyoudu <876955293@qq.com>
2022-05-23 16:11:04 +0800

    Update testexcel.py

--- testpython/testexcel.py
+++ testpython/testexcel.py
@@ -9,12 +9,21 @@ DDL1=DDL(r'D:\下载\CUST\00422-OM-(OM.OM.OM_GRP_IMS_NUM_PICK_HIS_EX.REQ_2022_03
 )
 for column in range(sheet.max_column+1):
     if column == 1:
-        sheet.cell(row=sheet.max_row+1,column=column).value=DDL1.Author
+        sheet.cell(row=sheet.max_row+1,column=column).value=DDL1.Author_1
     if column == 2:
-        sheet.cell(row=sheet.max_row,column=column).value= DDL1.Story
+        sheet.cell(row=sheet.max_row,column=column).value= DDL1.Author_1
     if column == 3:
         sheet.cell(row=sheet.max_row,column=column).value= DDL1.Remark
-    if column == 4:
-        sheet.cell(row=sheet.max_row,column=column).value= DDL1.DDLContent
-
+    if column == 5:
+        sheet.cell(row=sheet.max_row,column=column).value= 'ddl'
+    if column == 7:
+        sheet.cell(row=sheet.max_row,column=column).value= DDL1.DDLAllContent
+    if column == 8:
+        sheet.cell(row=sheet.max_row,column=column).value= DDL1.Author_2
+    if column == 11:
+        sheet.cell(row=sheet.max_row,column=column).value= '江苏bes'
+    if column == 14:
+        sheet.cell(row=sheet.max_row,column=column).value= '执行一次'    
+    if column == 15:
+        sheet.cell(row=sheet.max_row,column=column).value= '一般'  
 wb.save('a.xlsx')
