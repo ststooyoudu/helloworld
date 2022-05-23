efa2c064d5f81d64309cf944571992390d5224eb
ststooyoudu <876955293@qq.com>
2022-05-23 15:28:18 +0800

    Update testZip.py

--- testpython/testZip.py
+++ testpython/testZip.py
@@ -10,34 +10,25 @@ def scan_file():
             unzip_it(f)
             delete(f)
             return f            
- 
 def unzip_it(f):
     newZip=zipfile.ZipFile(f)
     newZip.extractall('.')  #解压到当前目录
     newZip.close()
-
 def delete(f):
     os.unlink(f)   #删除压缩包
-
 def zip_filename():
     for f in os.listdir():
         if f.endswith('.sql'):
             FileList.append(Path.cwd()/f)
-            
-
 #print(scan_file())
 print(Path.home())
 print(os.path)
 print(Path.cwd())
-
-
-
 while True:
     if scan_file():
         print('解压正常')
     else:
         print('解压完成！')
         break
-
 zip_filename()
 print(FileList)
