1c3fea915ee8a6e68bda6f9ba937051b6d3c608c
ststooyoudu <876955293@qq.com>
2022-05-23 15:07:57 +0800

    Update testZip.py

--- testpython/testZip.py
+++ testpython/testZip.py
@@ -2,23 +2,27 @@ import os
 import shutil
 import time
 import zipfile
-def scan_file():
-    for f in os.listdir(): #由于这里是当前路径，所以需要把这个代码文件和你要处理的文件放到同一个文件夹里
-        if f.endswith('.zip'):
-            newZip=zipfile.ZipFile(f)
-            newZip.extractall('.')       #解压到当前目录
-            os.unlink(f)                 #删除压缩包
-            return f
 
+def scan_file():
+    for f in os.listdir():  #由于这里是当前路径，所以需要把这个代码文件和你要处理的文件放到同一个文件夹里
+        if f.endswith('.zip'):
+            unzip_it(f)
+            delete(f)
+            return f            
+ 
 def unzip_it(f):
-    folder_name = f.split('.')[0]
-    target_path = os.path.join('.',folder_name)
-    os.makedirs(target_path)
-    shutil.unpack_archive(f,target_path)
+    newZip=zipfile.ZipFile(f)
+    newZip.extractall('.')  #解压到当前目录
+    newZip.close()
 
 def delete(f):
-    os.remove(f)
-    time.sleep(2)
-
+    os.unlink(f)   #删除压缩包
 
 print(scan_file())
+
+while True:
+    if scan_file():
+        print('解压正常')
+    else:
+        print('解压完成！')
+        break
