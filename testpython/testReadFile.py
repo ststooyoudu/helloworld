e0efad1251994e1a36871186352c469c3e260e85
ststooyoudu <876955293@qq.com>
2022-05-25 11:32:20 +0800

    Update testReadFile.py

--- testpython/testReadFile.py
+++ testpython/testReadFile.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 os.getcwd()
-file=r'/workspace/helloworld/00473-IM-(IM.OM.IM_RESERVE_BUSI_HIS_EX.REQ_2022_0206060_lwx997552.20220322103836.DDL)-Jiangsu_bes-swx552311.sql'
+file=r'/workspace/helloworld/testpython/00473-IM-(IM.OM.IM_RESERVE_BUSI_HIS_EX.REQ_2022_0206060_lwx997552.20220322103836.DDL)-Jiangsu_bes-swx552311.sql'
 # userName = ((file.split('\\')[-1]).split('-'))[1]  #从文件里提取用户名
 class DDL:
     def __init__(self,file):
@@ -23,8 +23,11 @@ class DDL:
         self.userName = ((str(file).split('/')[-1]).split('-'))[1]   
         self.Alist=Alist
         self.Create = (Alist[11].split(' ')[0].lower()=='create')   #判断文件是否是create
+        self.isTable = (Alist[11].split(' ')[1].lower()=='table')
         self.userDict = {'OM':['orders','crmdb1-4'],'IM':['im','resdb'],'PM':['prod','pubdb'],'SUBSCRIPTION':['subs','crmdb1-4'],'BESCHARGE':['bescharge','crmdb1-4'],'BESPRO':['bespro','pubdb'],'BESCUST':['cust','pubdb'],'COMMON':['sysmgr','pubdb'],'SM':['sysmgr','pubdb']}
         for k,v in self.userDict.items():
             if self.userName==k:
                 self.db_username=v[0]       #数据库用户名
                 self.db_name=v[1]          #数据库名
+        if self.Create and self.isTable:  #如果是新增表，记录新增表名
+            self.tablename=Alist[11].split(' ')[2]
