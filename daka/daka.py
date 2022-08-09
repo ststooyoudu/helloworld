ce768c79b2ef739760669acee46bebc920c7c244
ststooyoudu <876955293@qq.com>
2022-08-09 10:30:05 +0800

    Update daka.py

--- daka/daka.py
+++ daka/daka.py
@@ -10,32 +10,36 @@ import pyautogui
 import schedule
 
 
-s =Service('C:\selenium\console-test\lib\chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+def load_chrome():
+    s =Service('C:\selenium\console-test\lib\chromedriver.exe')
+    driver = webdriver.Chrome(service=s)
+    return driver
+
 account_items={'sunxing':'3edc#EDC','mengyuhao':'1qaz@WSX'}
 
 
-def click_button(value): #点击函数
+def click_button(value,driver): #点击函数
     click_name = driver.find_element(by=By.XPATH, value=value)
     ActionChains(driver).click(click_name).perform()
     time.sleep(2)
-def send_text(value,text):   #输入函数
+def send_text(value,text,driver):   #输入函数
     send_name = driver.find_element(by=By.XPATH, value=value).send_keys(text)
 #    ActionChains(driver).click(send_name).perform()
     time.sleep(2)
 def check_in(user,passwd):
     ##########################进入主站
     if check_net() == 200:
-        driver.get('http://xxxx.com')
+        driver = load_chrome()
+        driver.get('http://xx.com')
         driver.maximize_window()
         time.sleep(4)
-        send_text('/html/body/div[2]/form/div[1]/div/input',user)
+        send_text('/html/body/div[2]/form/div[1]/div/input',user,driver)
         time.sleep(2)
-        send_text('//*[@id="password"]',passwd)
-        click_button('/html/body/div[2]/form/div[4]/button')
-        click_button('//*[@id="workIn"]')
+        send_text('//*[@id="password"]',passwd,driver)
+        click_button('/html/body/div[2]/form/div[4]/button',driver)
+        click_button('//*[@id="workIn"]',driver)
         time.sleep(2)
-        click_button('/ html / body / div[1] / div[1] / div / div[4] / a / font') #退出登录下一个用户
+        click_button('/ html / body / div[1] / div[1] / div / div[4] / a / font',driver) #退出登录下一个用户
         time.sleep(3)
 
 #        driver.quit()
@@ -45,14 +49,15 @@ def check_in(user,passwd):
 def check_out(user,passwd):
     ##########################进入主站
     if check_net() == 200:
-        driver.get('http://xxxxxx.com')
+        driver = load_chrome()
+        driver.get('http://xx.com')
         driver.maximize_window()
         time.sleep(1)
-        send_text('/html/body/div[2]/form/div[1]/div/input',user)
-        send_text('//*[@id="password"]',passwd)
-        click_button('/html/body/div[2]/form/div[4]/button') #登录
+        send_text('/html/body/div[2]/form/div[1]/div/input',user,driver)
+        send_text('//*[@id="password"]',passwd,driver)
+        click_button('/html/body/div[2]/form/div[4]/button',driver) #登录
         current_handle = driver.current_window_handle #获取当前窗口
-        click_button('//*[@id="attendanceDiv"]/div[1]/div[3]')  #日报
+        click_button('//*[@id="attendanceDiv"]/div[1]/div[3]',driver)  #日报
         all_handles = driver.window_handles
         for handle in all_handles:
             if handle !=current_handle:
@@ -63,20 +68,19 @@ def check_out(user,passwd):
         print(current_handle)
         print(all_handles)
         print(now_handle)
-        click_button('//*[@id="createWorkDailyItem2"]')  # 任务
-        send_text('//*[@id="taskContent"]', '1、主干构建\n2、测试环境维护')
-        click_button('/ html / body / div[3] / div[1] / button[1]')  #保存
-        click_button('//*[@id="layui-layer1"]/div[3]/a')  #保存确定
-#        click_button('/ html / body / div[3] / div[1] / button[2] / span') #提交
+        click_button('//*[@id="createWorkDailyItem2"]',driver)  # 任务
+        send_text('//*[@id="taskContent"]', '1、主干构建\n2、测试环境维护',driver)
+        click_button('/ html / body / div[3] / div[1] / button[1]',driver)  #保存
+        click_button('//*[@id="layui-layer1"]/div[3]/a',driver)  #保存确定
+#        click_button('/ html / body / div[3] / div[1] / button[2] / span',driver) #提交
         driver.switch_to.window(current_handle) #切回主窗口
-        click_button('//*[@id="workOut"]') #签入
-        click_button('/ html / body / div[5] / div / div / div[2] / button[2]')  #确定
+        click_button('//*[@id="workOut"]',driver) #签入
+        click_button('/ html / body / div[5] / div / div / div[2] / button[2]',driver)  #确定
         time.sleep(2)
-        click_button('/ html / body / div[1] / div[1] / div / div[4] / a / font')  # 退出登录下一个用户
+        click_button('/ html / body / div[1] / div[1] / div / div[4] / a / font',driver)  # 退出登录下一个用户
         time.sleep(3)
     else:
         disconnect_net()
-
 
             
 #######################################监控网络
@@ -87,7 +91,7 @@ def check_net():
         requests.packages.urllib3.disable_warnings()  #忽视警告
         ss = requests.session()
         ss.keep_alive = False
-        response = requests.get("http://xxxx.com",verify=False)
+        response = requests.get("http://xx.com",verify=False)
         if response.status_code == 200:
             print("网络正常！")
             return 200
@@ -123,7 +127,7 @@ def all_checkout():
 
 ############################设置定时任务
 pyautogui.FAILSAFE = False
-schedule.every().day.at("08:26").do(all_checkin)
+schedule.every().day.at("10:29").do(all_checkin)
 schedule.every().day.at("18:11").do(all_checkout)
 #all_checkin()
 #all_checkout()
