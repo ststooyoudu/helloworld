9bd5501e35efe6451d74d33bd696c2eb5c361155
ststooyoudu <876955293@qq.com>
2022-09-27 09:40:50 +0800

    Create config.py

---
+++ python_create_package/config.py
@@ -0,0 +1,29 @@
+# paas地址
+baseURL = ''
+account = ''
+password = ''
+passwordForCeshi = ''
+tenantCode = ''
+# 认证中心URL
+authURL = ''
+# 申请token
+tokenApplyURL = ''
+tokenForCeshi = ''
+grant_type = 'client_credentials'
+client_id = 'pass'
+client_secret = ''
+scope = 'default_scope'
+# API网关URL
+gateway = ''
+tokenURL = ''
+refreshURL = ''
+#流水线API
+cd_pipline = ''
+#用户密码paas加密
+username = ''
+password = ''
+#sftp地址
+sftpAddr = ''
+#列表信息查询
+page_list = ''
+check_zip = ''
