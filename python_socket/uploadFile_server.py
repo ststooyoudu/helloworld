0031efd47af0f827712dadeef5c4b2af6ecaf8fa
ststooyoudu <876955293@qq.com>
2023-04-22 22:46:22 +0800

    Create uploadFile_server.py

---
+++ python_socket/uploadFile_server.py
@@ -0,0 +1,55 @@
+import socket, os,tqdm
+
+# 传输数据分隔符
+separator = "<separator>"
+
+# 服务器信息
+server_host = "127.0.0.1"
+server_port = 5002   # 1~1024多数会被系统占用，不建议用
+
+# 文件传输的缓冲区（传输不是一个字节一个字节传，而是一整个buffer）
+buffer_size = 4096
+
+# 创建的Server
+s = socket.socket()
+s.bind((server_host, server_port))  # 服务器绑定端口
+
+# 设置连接监听数
+s.listen(5)
+print(f"服务器端监听{server_host}:{server_port}")
+
+# 接受客户端连接
+client_socket, address = s.accept()     # 哪一个客户的socket, ip地址
+print(f"客户端{address}连接。")# 打印客户端ip
+
+# 接收客户端信息
+received = client_socket.recv(buffer_size).decode()     # 解码
+filename, file_size = received.split(separator)         # 客户端用separator分割的
+filename = os.path.basename(filename)                   # 获取文件的名字，去除路径
+file_size = int(file_size)      # 传输过来的是字符串类型
+
+# 文件接收处理
+progress = tqdm.tqdm(range(file_size), f"接收{filename}", unit="B", unit_divisor=1024, unit_scale=True)
+
+with open(filename, "wb") as f:     # 写入
+    for _ in progress:
+        # 从客户端读取数据
+        bytes_read = client_socket.recv(buffer_size)
+        if not bytes_read:      # 读取结束
+            break
+
+        f.write(bytes_read)
+        progress.update(len(bytes_read))
+
+# 关闭资源，先关闭客户端，再关闭服务器
+client_socket.close()
+s.close()
+
+
+
+
+
+
+
+
+
